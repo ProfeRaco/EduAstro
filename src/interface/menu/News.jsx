d748159283e23eea7b51bfb4e11bbf96b9d43138
seraco <sebastiaramon_1994@hotmail.com>
2017-11-12 09:30:04 +0000

    twitter news added!

--- src/interface/menu/News.jsx
+++ src/interface/menu/News.jsx
@@ -2,9 +2,16 @@
 import React from 'react';
 
 function News(props) {
+  const ht = window.innerHeight - 130
+  console.log(ht)
   return (
-    <div>
-      News will go here
+    <div style={{height: `${ht}px`, overflow: 'scroll'}}>
+      <a
+        className="twitter-timeline"
+        href="https://twitter.com/stella0chaos/lists/space-and-science?ref_src=twsrc%5Etfw"
+      >
+        A Twitter List by stella0chaos
+      </a>
     </div>
   );
 }
